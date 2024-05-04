ffa53c2854dd5781849db88cca4db5e66bc4ee07
Harold <harold.goldensky@gmail.com>
2024-05-04 18:13:16 +0800

    Update LicenseKey.js

--- src/Components/LicenseKey/LicenseKey.js
+++ src/Components/LicenseKey/LicenseKey.js
@@ -8,7 +8,7 @@ function LicenseKey() {
     const insertKey = (e)=>{
         e.preventDefault()
         // https://cautious-dog-gabardine.cyclic.app/
-        axios.post('https://dark-tam-hen.cyclic.app/api/auth/key', {
+        axios.post('https://backend-d9o6.onrender.com/api/auth/key', {
             key: key
         })
 
